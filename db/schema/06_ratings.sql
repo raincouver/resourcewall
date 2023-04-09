d0705f421605b4c53cdc960be4ac55f1333027a2
Kevin Cho <kevcho604@gmail.com>
2023-04-09 16:15:02 -0700

    Remove commas after certain lines to fix errors

--- db/schema/06_ratings.sql
+++ db/schema/06_ratings.sql
@@ -4,5 +4,5 @@ CREATE TABLE ratings (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
   resource_id INTEGER REFERENCES resources(id),
-  rating SMALLINT NOT NULL DEFAULT 0,
+  rating SMALLINT NOT NULL DEFAULT 0
 ); 
