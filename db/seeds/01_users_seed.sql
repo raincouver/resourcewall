81c17dd2d38bc142c43f00d925e0884b459e9d47
Kevin Cho <kevcho604@gmail.com>
2023-04-09 16:13:48 -0700

    Add seeds to all tables, test query for resources

---
+++ db/seeds/01_users_seed.sql
@@ -0,0 +1,121 @@
+-- Users table seeds here (Example)
+
+INSERT INTO users 
+  (
+    name, 
+    email, 
+    password, 
+    profile_picture_path
+  ) 
+VALUES 
+  (
+    'Valkyrie', 
+    'northstar@gmail.com', 
+    '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+    'resourcewall/public/images/trimmed/avatar1.PNG'
+  );
+
+INSERT INTO users 
+  (
+    name, 
+    email, 
+    password, 
+    profile_picture_path
+  ) 
+VALUES 
+  (
+    'Dr. Mary Somers', 
+    'adastra@gmail.com', 
+    '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+    'resourcewall/public/images/trimmed/avatar2.PNG'
+  );
+
+INSERT INTO users 
+  (
+    name, 
+    email, 
+    password, 
+    profile_picture_path
+  ) 
+VALUES 
+  (
+    'Ramya Parekh', 
+    'rampart@gmail.com', 
+    '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+    'resourcewall/public/images/trimmed/avatar3.PNG'
+  );
+
+INSERT INTO users 
+  (
+    name, 
+    email, 
+    password, 
+    profile_picture_path
+  ) 
+VALUES 
+  (
+    'Rene H. Blasey', 
+    'wraith@gmail.com', 
+    '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+    'resourcewall/public/images/trimmed/avatar4.PNG'
+  );
+
+  INSERT INTO users 
+  (
+    name, 
+    email, 
+    password, 
+    profile_picture_path
+  ) 
+VALUES 
+  (
+    'Dr. Milo', 
+    'docmilo@gmail.com', 
+    '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+    'resourcewall/public/images/trimmed/avatar5.PNG'
+  );
+
+INSERT INTO users 
+  (
+    name, 
+    email, 
+    password, 
+    profile_picture_path
+  ) 
+VALUES 
+  (
+    'Ashleigh Reid', 
+    'ash@gmail.com', 
+    '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+    'resourcewall/public/images/trimmed/avatar6.PNG'
+  );
+
+INSERT INTO users 
+  (
+    name, 
+    email, 
+    password, 
+    profile_picture_path
+  )
+VALUES 
+  ( 
+    'Brigitte', 
+    'shieldknight@gmail.com', 
+    '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 
+    'resourcewall/public/images/trimmed/avatar7.PNG'
+  );
+
+INSERT INTO users 
+  (
+    name, 
+    email, 
+    password, 
+    profile_picture_path
+  )
+VALUES 
+  ( 
+    'Hachiko', 
+    'hachiko@gmail.com', 
+    '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 
+    'resourcewall/public/images/trimmed/avatar8.PNG'
+  );
---
+++ db/seeds/02_categories_seed.sql
@@ -0,0 +1,11 @@
+-- Category 1 -- 'Coding' 
+INSERT INTO categories (name)
+VALUES ('Coding');
+
+-- Category 2 -- 'General' 
+INSERT INTO categories (name)
+VALUES ('General');
+
+-- Category 3 -- 'Hobbies'
+INSERT INTO categories (name)
+VALUES ('Hobbies');
