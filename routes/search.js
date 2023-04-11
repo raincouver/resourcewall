9e3c52fb3b1ffeb118265298e2f0e0c44d6a5c80
Kevin Cho <kevcho604@gmail.com>
2023-04-11 11:14:37 -0700

    Add search route, ejs page, script, stylesheet

---
+++ routes/search.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router  = express.Router();
+const { searchAllResources } = require('../db/database');
+
+router.get('/', (req, res) => {
+  console.log('hey');
+  res.render('search');
+});
+
+router.post('/', (req, res) => {
+  const searchTerm = req.body.searchTerm;
+
+  console.log('Search term:', searchTerm) // debugging 
+  
+  searchAllResources(`%${searchTerm}%`)
+    .then((results) => {
+      res.send(results);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(err);
+    });
+});
+
+module.exports = router;
