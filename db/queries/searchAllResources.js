9bb7476c2a6ef64b70a9dcaa0089f5a70006dc6e
Kevin Cho <kevcho604@gmail.com>
2023-04-11 15:12:02 -0700

    Round average_rating to 1 decimal place, return url

--- db/queries/searchAllResources.js
+++ db/queries/searchAllResources.js
@@ -6,7 +6,8 @@ const searchAllResources = (searchInput) => {
   return db
     .query(
       `SELECT resources.title, 
-      avg(ratings.rating) as average_rating
+      resources.url, 
+      ROUND(AVG(ratings.rating), 1) as average_rating
       FROM resources
       JOIN ratings ON resources.id = resource_id
       JOIN users ON resources.user_id = users.id
