c8db577e0749bc38a21b2b1f8465b68c59016354
Kevin Cho <kevcho604@gmail.com>
2023-04-12 20:35:01 -0700

    Add rating system db injection

--- public/scripts/resource-star-rating.js
+++ public/scripts/resource-star-rating.js
@@ -1,7 +1,6 @@
-// Helper script to make star rating interactive
-
 const allStars = document.querySelectorAll('.star')
 const currentRating = document.querySelector('.current-rating');
+const ratingElement = document.getElementById('rating');
 
 allStars.forEach((star, i) => {
   star.onclick = function() {
@@ -13,6 +12,24 @@ allStars.forEach((star, i) => {
         star.innerHTML = '&#9734';
       }
     })
+    ratingElement.value = current_star_level; 
     currentRating.textContent = `${current_star_level} of 5`;
+
+    // AJAX request to the server to add the rating
+    const id = window.location.pathname.slice(-1)
+    
+    $.ajax({
+      url: `/resource-modify/rate/${id}`,
+      type: 'POST',
+      data: {
+        rating: current_star_level
+      },
+      success: function(response) {
+        console.log(response);
+      },
+      error: function(error) {
+        console.log(error);
+      }
+    });
   }
 })
